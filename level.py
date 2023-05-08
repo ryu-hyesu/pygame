89bc87e833a2dbe8350dbf4da9d2e403b932262c
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-05-08 20:54:30 +0900

    init setting completed!

---
+++ level.py
@@ -0,0 +1,22 @@
+import pygame
+from settings import *
+
+class Level :
+    def __init__(self):
+        # this is the same of surface of main.py
+        # this allows the level to draw straight on the main display tha we are going to display to the player
+        # makes organizing the entire thing
+        
+        # get the display surface
+        self.display_surface = pygame.display.get_surface()
+
+        # sprite groups
+        # this help us to draw and update any kind of sprite
+        self.all_sprites = pygame.sprite.Group()
+    
+    def run(self, dt) :
+        # we can run the entire game inside of it
+        # which is helping considerabley to keep the entire clean and organized
+        self.display_surface.fill('black')
+        self.all_sprites.draw(self.display_surface)
+        self.all_sprites.update()
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pygame, sys
 from settings import *
+from level import Level
 
 class Game :
     def __init__(self) -> None:
         pygame.init()
+        # creating a display surface & clock
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Sprout land")
         self.clock = pygame.time.Clock()
+        self.level = Level()
     
-    def run(self):
+    # most of game is going to run inside of the run method
+    # check game loop
+    def run(self): 
         while True:
+            # close game
             for event in pygame.event.get() :
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.exit()
-                    
+                    sys.exit() # 게임 종료
+                
+                # get delta time  
                 dt = self.clock.tick() / 1000
+                self.level.run(dt)
+                # update game
                 pygame.display.update()
 
 if __name__ == '__main__' :
