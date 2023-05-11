07c7e26b9b700da29873ce26373b1327ed936307
ryu-hyesu <83686474+ryu-hyesu@users.noreply.github.com>
2023-05-11 12:11:30 +0900

    파일 정리

--- code/player.py
+++ code/player.py
@@ -14,6 +14,17 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
         
+    def import_assets(self) :
+        self.animations = {'up' : [], 'down' : [], 'left' : [], 'right' : [],
+                           'right_idle' : [], 'left_idle' : [], 'up_idle' : [], 'down_idle' : [],
+                           'right_hoe' : [], 'left_hoe' : [], 'up_hoe' : [], 'down_hoe' : [],
+                           'right_axe' : [], 'left_axe' : [], 'up_axe' : [], 'down_axe' : [],
+                           'right_water' : [], 'left_water' : [], 'up_water' : [], 'down_water' : []}
+        
+        for animation in self.animations.keys() :
+            full_path = '../graphics/character/' + animation
+        
+        
     def input(self) :
         # list with all of the keys that are potentially being pressed
         keys = pygame.key.get_pressed()
@@ -42,13 +53,12 @@ class Player(pygame.sprite.Sprite):
         
         # horizontal movement
         self.pos.x += self.direction.x * self.speed * dt 
-        self.rect.center.x = self.pos.x
+        self.rect.centerx = self.pos.x
         
         # vertical movement
         self.pos.y += self.direction.y * self.speed * dt 
-        self.rect.center.y = self.pos.y
+        self.rect.centery = self.pos.y
         
-
     # all_sprites's update is run ofcourse this
     def update(self, dt) :
         self.input()
